70e78248edd220cfd0a896ddc3a28e8c6186f838
LDChristian <77200735+LDChristian@users.noreply.github.com>
2021-12-29 00:53:04 -0500

    Cardiologia Links

--- cardiologia.php
+++ cardiologia.php
@@ -28,7 +28,7 @@
 <header>
 	<nav class="navbar navbar-expand-lg navigation" id="navbar">
 		<div class="container">
-		 	 <a class="navbar-brand" href="index.html">
+		 	 <a class="navbar-brand" href="index.php">
 				<img width="180px" height="60px" src="images/logo_large.png" alt="" class="img-fluid">
 			  </a>
 
@@ -39,35 +39,31 @@
 		  <div class="collapse navbar-collapse" id="navbarmain">
 			<ul class="navbar-nav ml-auto">
 			  <li class="nav-item active">
-				<a class="nav-link" href="index.html">Inicio</a>
+				<a class="nav-link" href="index.php">Inicio</a>
 			  </li>
-			   <li class="nav-item"><a class="nav-link" href="about.html">Sobre Nosotros</a></li>
-			    <li class="nav-item"><a class="nav-link" href="service.html">Especialidades</a></li>
+			   <li class="nav-item"><a class="nav-link" href="nosotros.php">Sobre Nosotros</a></li>
+			    <li class="nav-item"><a class="nav-link" href="especialidades.php">Especialidades</a></li>
 
 			    <li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Departamentos <i class="icofont-thin-down"></i></a>
+					<a class="nav-link dropdown-toggle" href="department.php" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Departamentos <i class="icofont-thin-down"></i></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdown02">
-						<li><a class="dropdown-item" href="department-single.html">Cardiología</a></li>
+						<li><a class="dropdown-item" href="cardiologia.php">Cardiología</a></li>
 					</ul>
 			  	</li>
 
 			  	<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctores <i class="icofont-thin-down"></i></a>
+					<a class="nav-link dropdown-toggle" href="doctor.php" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctores <i class="icofont-thin-down"></i></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdown03">
-						<li><a class="dropdown-item" href="doctor.html">Directivos</a></li>
-						<li><a class="dropdown-item" href="doctor-single.html">Doctor Individual</a></li>
-						<li><a class="dropdown-item" href="appoinment.html">Citas</a></li>
+						<li><a class="dropdown-item" href="doctor.php">Directivos</a></li>
+						<li><a class="dropdown-item" href="appoinment.php">Citas</a></li>
 					</ul>
 			  	</li>
-			   <li class="nav-item"><a class="nav-link" href="contact.html">Contáctanos</a></li>
+			   <li class="nav-item"><a class="nav-link" href="contactanos.php">Contáctanos</a></li>
 			</ul>
 		  </div>
 		</div>
 	</nav>
 </header>
-	
-
-
 <section class="page-title bg-1">
   <div class="overlay"></div>
   <div class="container">
@@ -78,7 +74,7 @@
           <h1 class="text-capitalize mb-5 text-lg">Cardiología</h1>
 
           <!-- <ul class="list-inline breadcumb-nav">
-            <li class="list-inline-item"><a href="index.html" class="text-white">Home</a></li>
+            <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
             <li class="list-inline-item"><span class="text-white">/</span></li>
             <li class="list-inline-item"><a href="#" class="text-white-50">Department Details</a></li>
           </ul> -->
@@ -89,12 +85,12 @@
 </section>
 
 
-<section class="section department-single">
+<section class="section cardiologia">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="department-img">
-					<img src="images/service/service-9.jpg" alt="" class="img-fluid">
+					<img src="images/especialidades/especialidades-9.jpg" alt="" class="img-fluid">
 				</div>
 			</div>
 		</div>
@@ -110,7 +106,7 @@
 
 					<h3 class="mt-5 mb-4">Servicios de Cardiología</h3>
 					<div class="divider my-4"></div>
-					<ul class="list-unstyled department-service">
+					<ul class="list-unstyled department-especialidades">
 						<li><i class="icofont-check mr-2"></i>Rehabilitacion Cardiaca</li>
 						<li><i class="icofont-check mr-2"></i>Monitoreo del ritmo cardiaco</li>
 						<li><i class="icofont-check mr-2"></i>Consulta General de Cardiología</li>
@@ -119,7 +115,7 @@
 						<li><i class="icofont-check mr-2"></i>Cateterizacion Cardiaca</li>
 					</ul>
 
-					<a href="appoinment.html" class="btn btn-main-2 btn-round-full">Reserve una cita<i class="icofont-simple-right ml-2  "></i></a>
+					<a href="appoinment.php" class="btn btn-main-2 btn-round-full">Reserve una cita<i class="icofont-simple-right ml-2  "></i></a>
 				</div>
 			</div>
 
@@ -177,36 +173,35 @@
 					<div class="divider mb-4"></div>
 
 					<ul class="list-unstyled footer-menu lh-35">
-						<li><a href="#">Cirugia </a></li>
-						<li><a href="#">Salúd de la Mujer</a></li>
-						<li><a href="#">Radiologia</a></li>
-						<li><a href="#">Cardiología</a></li>
-						<li><a href="#">Medicina General</a></li>
+						<li><a href="medicina-general.php">Medicina General</a></li>
+						<li><a href="cardiologia.php">Cardiología</a></li>
+						<li><a href="neurologia.php">Neurología</a></li>
+						<li><a href="pediatria.php">Pediatría</a></li>
+						<li><a href="dermatologia.php">Dermatología</a></li>
 					</ul>
 				</div>
 			</div>
-
 			<div class="col-lg-2 col-md-6 col-sm-6">
 				<div class="widget mb-5 mb-lg-0">
-					<h4 class="text-capitalize mb-3">Soporte</h4>
+					<h4 class="text-capitalize mb-3">Nosotros</h4>
 					<div class="divider mb-4"></div>
 
 					<ul class="list-unstyled footer-menu lh-35">
-						<li><a href="#">Terminos y condiciones</a></li>
-						<li><a href="#">Política de privacidad</a></li>
-						<li><a href="#">Soporte de la Empresa </a></li>
-						<li><a href="#">Preguntas Frecuentes</a></li>
-						<li><a href="#">Licencia de Compañia</a></li>
+						<li><a href="about.php">Términos & Condiciones</a></li>
+						<li><a href="about.php">Política de Privacidad</a></li>
+						<li><a href="about.php">Apoyo de la Compañía </a></li>
+						<li><a href="about.php">Preguntas & Respuestas</a></li>
+						<li><a href="about.php">Licencia de la compañía</a></li>
 					</ul>
 				</div>
 			</div>
 
 			<div class="col-lg-3 col-md-6 col-sm-6">
-				<div class="widget widget-contact mb-5 mb-lg-0">
-					<h4 class="text-capitalize mb-3">Ponerse en Contacto</h4>
+				<div class="widget widget-contactanos mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Ponerse en contactanoso</h4>
 					<div class="divider mb-4"></div>
 
-					<div class="footer-contact-block mb-4">
+					<div class="footer-contactanos-block mb-4">
 						<div class="icon d-flex align-items-center">
 							<i class="icofont-email mr-3"></i>
 							<span class="h6 mb-0">Soporte disponible 24/7</span>
@@ -214,7 +209,7 @@
 						<h4 class="mt-2"><a href="tel:+23-345-67890">Soporte@email.com</a></h4>
 					</div>
 
-					<div class="footer-contact-block">
+					<div class="footer-contactanos-block">
 						<div class="icon d-flex align-items-center">
 							<i class="icofont-support mr-3"></i>
 							<span class="h6 mb-0">Lunes a Viernes : 08:30 - 18:00</span>
@@ -277,7 +272,7 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>    
     
     <script src="js/script.js"></script>
-    <script src="js/contact.js"></script>
+    <script src="js/contactanos.js"></script>
 
   </body>
-  </html>
+  </php>
