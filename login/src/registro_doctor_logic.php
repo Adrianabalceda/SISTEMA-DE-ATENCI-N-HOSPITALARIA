71322444468cda0da0d905912c934ff98d1bfe42
Leonardo Dario Loaiza Sighuas <leonardodario.loaiza@unmsm.edu.pe>
2021-12-29 14:34:29 -0500

    Update registro_doctor_logic.php

--- login/src/registro_doctor_logic.php
+++ login/src/registro_doctor_logic.php
@@ -6,7 +6,6 @@
     $email = $_POST['email'];
     $dni = $_POST['dni'];
     $especialidad_id = $_POST['especialidad'];
-
     $doctor_id = stripcslashes($dni);
     $usuario = strtoupper($nombres.' '.$apellidos);
    
@@ -35,14 +34,9 @@
     $sql = "SELECT * FROM `doctor` WHERE id = '$doctor_id'";
     $result = mysqli_query($conexion, $sql);
     $count2 = mysqli_num_rows($result);
-
-    $sql = "SELECT * FROM `asegurado` WHERE id = '$doctor_id'";
-    $result = mysqli_query($conexion, $sql);
-    $count3 = mysqli_num_rows($result);
-
    
 
-    if($count < 1 && $count2 < 1 && $count3 < 1){
+    if($count < 1 && $count2 < 1){
         $sql = "INSERT INTO `doctor` (`id`, `usuario`, `contraseña`, `nombres`, 
         `apellidos`, `id_especialidad`, `email`, `celular`) 
         VALUES ('$doctor_id','$usuario','$doctor_id','$nombres','$apellidos','$especialidad_id','$email','$celular')";
@@ -55,7 +49,7 @@
         if($count == 1){
             header("location: ../registro_doctor.php?error=1");
         }
-        if($count2 == 1 || $count3 == 1){
+        if($count2 == 1){
             header("location: ../registro_doctor.php?error=2");
         }
 
