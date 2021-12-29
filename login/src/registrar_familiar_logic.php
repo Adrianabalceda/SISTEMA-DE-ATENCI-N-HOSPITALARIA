70fe0de42f6c93d90c2a367611c15c3697b34fa3
Leonardo Dario Loaiza Sighuas <leonardodario.loaiza@unmsm.edu.pe>
2021-12-29 17:14:32 -0500

    cambios

--- login/src/registrar_familiar_logic.php
+++ login/src/registrar_familiar_logic.php
@@ -1,45 +1,35 @@
 <?php
     include('conexion_db.php');
-    
     $nombres = $_POST['nombresFamiliar'];
     $apellidos = $_POST['apellidosFamiliar'];
     $dni = $_POST['dniFamiliar'];
     $id_asegurado = $_POST['dniasegurado'];
     $email = $_POST['email'];
     $fecha = $_POST['fecha'];
-    $dni = stripcslashes($dniAsegurado);
-
-
+    $dni = stripcslashes($dni);
 
     $sql = "SELECT * FROM `asegurado` WHERE id = '$dni'";
     $result = mysqli_query($conexion, $sql);
     $count = mysqli_num_rows($result);
-    
-    $sql = "SELECT * FROM `doctor` WHERE id = '$dni'";
-    $result = mysqli_query($conexion, $sql);
-    $count2 = mysqli_num_rows($result);
 
     $sql = "SELECT * FROM `familiar` WHERE id_familiar = '$dni'";
     $result = mysqli_query($conexion, $sql);
-    $count3 = mysqli_num_rows($result);
+    $count2 = mysqli_num_rows($result);
 
-    
 
-    if($count !== 1 && $count2 !== 1 && $count3 !== 1) {
-        
-        $sql = "INSERT INTO `familiar` ('id_familiar',`DNI`,'id_asegurado', `nombres`, `apellidos`, `email`,'fecha_nac') 
-        VALUES ('$dni','$dni','$id_asegurado','$nombres', '$apellidos', '$email','$fechaAsegurado')";
-        
+
+    if ($count !== 1 && $count2 !== 1) {
+        $sql = "INSERT INTO `familiar` ('id_familiar',`DNI`,'id_asegurado', `nombres`, `apellidos`, `email`,`fecha_nac`) 
+            VALUES ('$dni','$dni','$id_asegurado','$nombres', '$apellidos', '$email','$fecha')";
+
         mysqli_query($conexion, $sql);
 
 
         header("location: ../AñadirFamiliar.php?mensaje=1");
-    }
-    else{
-        if($count == 1 || $count2 == 1 || $count3 == 1){
+    } else {
+        if ($count == 1 || $count2 == 1) {
             header("location: ../AñadirFamiliar.php?error=1");
         }
-      
     }
-    
+
 ?>
