43f032f506df2f669ad035db48b584df021936ac
altoma123 <tokkto.jr@hotmail.com>
2021-12-28 23:29:24 -0500

    pediatria en espaniol

---
+++ pediatria.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
+  <meta name="author" content="themefisher.com">
+
+  <title>Medifind - Sistema de atención hospitalaria</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  <!-- Icon Font Css -->
+  <link rel="stylesheet" href="plugins/icofont/icofont.min.css">
+  <!-- Slick Slider  CSS -->
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body id="top">
+
+<header>
+	<div class="header-top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<ul class="top-bar-info list-inline-item pl-0 mb-0">
+						<li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>plat_online@medifind.com</a></li>
+						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Av. Canto Grande, San Juan de Lurigancho</li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+						<a href="tel:+23-345-67890" >
+							<span>Llámanos : </span>
+							<span class="h4">555-5555</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+		 	 <a class="navbar-brand" href="index.html">
+			  	<img src="images/logo.png" alt="" class="img-fluid">
+			  </a>
+
+		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="icofont-navigation-menu"></span>
+		  </button>
+	  
+		  <div class="collapse navbar-collapse" id="navbarmain">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="index.html">Home</a>
+			  </li>
+			   <li class="nav-item"><a class="nav-link" href="about.html">Sobre nosotros</a></li>
+			    <li class="nav-item"><a class="nav-link" href="service.html">Especialidades</a></li>
+
+			    <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Departamentos <i class="icofont-thin-down"></i></a>
+					<ul class="dropdown-menu" aria-labelledby="dropdown02">
+						<li><a class="dropdown-item" href="department.html">Medicina General</a></li>
+						<li><a class="dropdown-item" href="department-single.html">Cardiología</a></li>
+						<li><a class="dropdown-item" href="department-single.html">Neurología</a></li>
+						<li><a class="dropdown-item" href="department-single.html">Pediatría</a></li>
+						<li><a class="dropdown-item" href="department-single.html">Dermatología</a></li>
+					</ul>
+			  	</li>
+
+			  	<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctores <i class="icofont-thin-down"></i></a>
+					<ul class="dropdown-menu" aria-labelledby="dropdown03">
+						<li><a class="dropdown-item" href="doctor.html">Directivos</a></li>
+						<li><a class="dropdown-item" href="appoinment.html">Citas</a></li>
+					</ul>
+			  	</li>
+
+			   
+			   <li class="nav-item"><a class="nav-link" href="contact.html">Contáctanos</a></li>
+			</ul>
+		  </div>
+		</div>
+	</nav>
+</header>
+	
+
+
+<section class="page-title bg-1">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="block text-center">
+          <span class="text-white">Detalles</span>
+          <h1 class="text-capitalize mb-5 text-lg">Pediatría</h1>
+
+          <!-- <ul class="list-inline breadcumb-nav">
+            <li class="list-inline-item"><a href="index.html" class="text-white">Home</a></li>
+            <li class="list-inline-item"><span class="text-white">/</span></li>
+            <li class="list-inline-item"><a href="#" class="text-white-50">Department Details</a></li>
+          </ul> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="section department-single">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="department-img">
+					<img src="images/service/bg-1.jpg" alt="" class="img-fluid">
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="department-content mt-5">
+					<h3 class="text-md">Pediatría</h3>
+					<div class="divider my-4"></div>
+					<p class="lead">Nuestro destacado staff médico es responsable del cuidado integral de la salud de tus hijos, desde antes de su llegada, hasta su adolescencia</p>
+					<p>Usando el juego y la comunicación como herramientas primarias, las intervenciones de la especialista facilitan la atención médica en eventos o procedimientos médicos y establecer relaciones terapéuticas con los niños y los padres para apoyar la participación de la familia en el cuidado de cada niño, con continuidad en su cuidado</p>
+
+
+					<h3 class="mt-5 mb-4">Detalles del servicio</h3>
+					<div class="divider my-4"></div>
+					<ul class="list-unstyled department-service">
+						<li><i class="icofont-check mr-2"></i>Control Pre Natal</li>
+						<li><i class="icofont-check mr-2"></i>Control Post natal</li>
+						<li><i class="icofont-check mr-2"></i>Control de Niño Sano</li>
+						<li><i class="icofont-check mr-2"></i>Estimulación para menores de 2 años</li>
+						<li><i class="icofont-check mr-2"></i>Urgencias pediátricas</li>
+						<li><i class="icofont-check mr-2"></i>Emergencias pediátricas</li>
+					</ul>
+
+					<a href="appoinment.html" class="btn btn-main-2 btn-round-full">Reserve una cita<i class="icofont-simple-right ml-2  "></i></a>
+				</div>
+			</div>
+
+			<div class="col-lg-4">
+				<div class="sidebar-widget schedule-widget mt-5">
+					<h5 class="mb-4">Horarios</h5>
+
+					<ul class="list-unstyled">
+					  <li class="d-flex justify-content-between align-items-center">
+					    <a href="#">Lunes a Viernes</a>
+					    <span>9:00 - 17:00</span>
+					  </li>
+					  <li class="d-flex justify-content-between align-items-center">
+					    <a href="#">Sábados</a>
+					    <span>9:00 - 16:00</span>
+					  </li>
+					  <li class="d-flex justify-content-between align-items-center">
+					    <a href="#">Domingos</a>
+					    <span>Cerrado</span>
+					  </li>
+					</ul>
+
+					<div class="sidebar-contatct-info mt-4">
+						<p class="mb-0">Necesita atención Urgente?</p>
+						<h3>555-5555</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- footer Start -->
+<footer class="footer section gray-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 mr-auto col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<div class="logo mb-4">
+						<img src="images/logo.png" alt="" class="img-fluid">
+					</div>
+					<p>Plataforma en línea puesta a disposición de los asegurados del Hospital San Juan.</p>
+
+					<ul class="list-inline footer-socials mt-4">
+						<li class="list-inline-item"><a href="https://www.facebook.com/themefisher"><i class="icofont-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="https://twitter.com/themefisher"><i class="icofont-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.pinterest.com/themefisher/"><i class="icofont-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Departamentos</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Medicina General</a></li>
+						<li><a href="#">Cardiología</a></li>
+						<li><a href="#">Neurología</a></li>
+						<li><a href="#">Pediatría</a></li>
+						<li><a href="#">Dermatología</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Sobre Nosotros</h4>
+					<div class="divider mb-4"></div>
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Términos y Condiciones</a></li>
+						<li><a href="#">Política de Privacidad</a></li>
+						<li><a href="#">Apoyo de la Compañía </a></li>
+						<li><a href="#">Preguntas y Respuestas</a></li>
+						<li><a href="#">Licencia de la compañía</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="widget widget-contact mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Contáctenos</h4>
+					<div class="divider mb-4"></div>
+
+					<div class="footer-contact-block mb-4">
+						<div class="icon d-flex align-items-center">
+							<i class="icofont-email mr-3"></i>
+							<span class="h6 mb-0">Asistencia disponible las 24 horas</span>
+						</div>
+						<h4 class="mt-2"><a href="tel:+23-345-67890">Contactenos@email.com</a></h4>
+					</div>
+
+					<div class="footer-contact-block">
+						<div class="icon d-flex align-items-center">
+							<i class="icofont-support mr-3"></i>
+							<span class="h6 mb-0">Lun a Vie : 08:30 - 18:00</span>
+						</div>
+						<h4 class="mt-2"><a href="tel:+23-345-67890">555 - 5555</a></h4>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-btm py-4 mt-5">
+			<div class="row align-items-center justify-content-between">
+				<div class="col-lg-6">
+					<div class="copyright">
+						&copy; Copyright Reserved to <span class="text-color">G3</span>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="subscribe-form text-lg-right mt-5 mt-lg-0">
+						<form action="#" class="subscribe">
+							<input type="text" class="form-control" placeholder="Escriba su correo aquí">
+							<a href="#" class="btn btn-main-2 btn-round-full">Suscribirse</a>
+						</form>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4">
+					<a class="backtop js-scroll-trigger" href="#top">
+						<i class="icofont-long-arrow-up"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+   
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+
+    
+    <!-- Main jQuery -->
+    <script src="plugins/jquery/jquery.js"></script>
+    <!-- Bootstrap 4.3.2 -->
+    <script src="plugins/bootstrap/js/popper.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="plugins/counterup/jquery.easing.js"></script>
+    <!-- Slick Slider -->
+    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+    <!-- Counterup -->
+    <script src="plugins/counterup/jquery.waypoints.min.js"></script>
+    
+    <script src="plugins/shuffle/shuffle.min.js"></script>
+    <script src="plugins/counterup/jquery.counterup.min.js"></script>
+    <!-- Google Map -->
+    <script src="plugins/google-map/map.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>    
+    
+    <script src="js/script.js"></script>
+    <script src="js/contact.js"></script>
+
+  </body>
+  </html>
